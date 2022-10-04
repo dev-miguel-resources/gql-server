bce2fe13e40a662069fa62b4ecee6297064455b2
dev-miguel-resources <ingenieromiguelch@gmail.com>
2022-10-04 20:24:58 -0300

    review in code with graphql

--- database/index.js
+++ database/index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.DATABASE_CLOUD, {
+    await mongoose.connect(process.env.DATABASE, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true,
